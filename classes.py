d3c49071d00bc9909f508a3ee77462a3dedca15b
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-09-28 07:55:53 -0300

    criação das primeiras classes

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Loja:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+    
+class Admin:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
+
+    
